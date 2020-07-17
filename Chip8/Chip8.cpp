e11b7fb82f12b014df4a559af0c79e3342f70ed9
badayvedat <badayvedat@gmail.com>
2020-07-17 23:23:15 +0300

    Opcode FX0A implemented.

--- Chip8/Chip8.cpp
+++ Chip8/Chip8.cpp
@@ -325,6 +325,23 @@ void Chip8::emulateCycle() {
 					delay_timer = V[(opcode & 0x0F00) >> 8];
 					pc += 2;
 					break;
+
+				// A key press is awaited, and then stored in VX. (Blocking Operation. All instruction halted until next key event).
+				case 0x000A:
+					bool key_pressed = false;
+					
+					while (!key_pressed) {
+						for (int i = 0; i < 16; i++) {
+							if (key[i] != 0) {
+								key_pressed = true;
+								V[(opcode & 0x0F00) >> 8] = i;
+								break;
+							}
+						}
+					}
+
+					pc += 2;
+					break;
 			}
 
 		default:
