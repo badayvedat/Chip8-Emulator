47331f9041dbb9ec634e554192e04d404b3e5c27
badayvedat <badayvedat@gmail.com>
2020-07-19 18:10:47 +0300

    Implementation of drawGraphics function moved from header file to implementation file.

--- Chip8/Platform.cpp
+++ Chip8/Platform.cpp
@@ -24,3 +24,27 @@ bool Platform::init(const char* title, int width, int height) {
 	return true;
 }
 
+void Platform::drawGraphics(unsigned char* display, const int width, const int height) {
+		SDL_Rect rect;
+		rect.x = 0;
+		rect.y = 0;
+		rect.w = 1;
+		rect.h = 1;
+
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 0);
+
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				if (display[(y * width) + x]) {
+					rect.x = x;
+					rect.y = y;
+
+					SDL_RenderFillRect(renderer, &rect);
+				}
+			}
+		}
+		SDL_RenderPresent(renderer);
+}
+
--- Chip8/Platform.h
+++ Chip8/Platform.h
@@ -7,31 +7,7 @@ public:
 	~Platform();
 
 	bool init(const char* title, int width, int height);
-	void drawGraphics(unsigned char* display, const int width, const int height) {
-		SDL_Rect rect;
-		rect.x = 0;
-		rect.y = 0;
-		rect.w = 1;
-		rect.h = 1;
-
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		SDL_RenderClear(renderer);
-		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 0);
-
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
-				if (display[(y * width) + x]) {
-					rect.x = x;
-					rect.y = y;
-
-					SDL_RenderFillRect(renderer, &rect);
-				}
-			}
-		}
-		SDL_RenderPresent(renderer);
-		// set drawFlag to false
-
-	}
+	void drawGraphics(unsigned char* display, const int width, const int height);
 
 private:
 	SDL_Window* window;
