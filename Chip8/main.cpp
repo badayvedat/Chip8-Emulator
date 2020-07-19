453e86d53743660b6b1990de05297493aebee178
badayvedat <badayvedat@gmail.com>
2020-07-19 18:08:30 +0300

    Platform header included in main. main signature changed for SDL2. Unnecessary comment lines removed.

--- Chip8/main.cpp
+++ Chip8/main.cpp
@@ -1,37 +1,23 @@
 #include "Chip8.h"
+#include "Platform.h"
 
-int main() {
-	// setupGraphics()
-	// setupInput()
+int main(int argc, char* argv[]) {
 	Chip8 chip8;
 	chip8.initialize();
 	chip8.loadGame("test_opcode.ch8");
-	for (int i = 0; i < 100; i++) {
+
+	Platform platform;
+	platform.init("TEST", 64, 32);
+
+	while (true) {
 		chip8.emulateCycle();
-	}
-	
-	/*
-		// Set up render system and register input callbacks
-		setupGraphics();
-		setupInput();
-
-		// Initialize the Chip8 system and load the game into the memory  
-		myChip8.initialize();
-		myChip8.loadGame("pong");
-
-		// Emulation loop
-		for (;;) {
-			// Emulate one cycle
-			myChip8.emulateCycle();
-
-			// If the draw flag is set, update the screen
-			if (myChip8.drawFlag)
-				drawGraphics();
-
-			// Store key press state (Press and Release)
-			myChip8.setKeys();
+		
+		if (chip8.getDrawFlag()) {
+			platform.drawGraphics(chip8.getGfx(), 64, 32);
 		}
+		
+		// setupInput()
 
-		return 0;
-	*/
+	}
+	return 0;
 }
