928441da9021f554df61e3e43aa040bfc09176b5
badayvedat <badayvedat@gmail.com>
2020-07-20 22:08:50 +0300

    Global SDL_Event variable removed from Platform.

--- Chip8/Platform.cpp
+++ Chip8/Platform.cpp
@@ -79,6 +79,8 @@ void Platform::drawGraphics(unsigned char* display, const int width, const int h
 }
 
 bool Platform::handleInput(unsigned char* key) {
+	SDL_Event event;
+
 	while (SDL_PollEvent(&event)) {
 		if (event.type == SDL_QUIT) {
 			return false;
--- Chip8/Platform.h
+++ Chip8/Platform.h
@@ -18,7 +18,6 @@ public:
 private:
 	SDL_Window* window;
 	SDL_Renderer* renderer;
-	SDL_Event event;
 
 	Mix_Chunk* beepSound;
 };
