62e36c0f9062733eb20c5fb00ecc7a09e8dd6e85
badayvedat <badayvedat@gmail.com>
2020-07-22 00:01:33 +0300

    ConfigManager declarations created.

--- Chip8/ConfigManager.cpp
+++ Chip8/ConfigManager.cpp
@@ -1,1 +1,25 @@
 #include "ConfigManager.h"
+
+bool ConfigManager::loadConfigFile() {
+    return false;
+}
+
+int ConfigManager::getCycleRefreshRate() {
+    return 0;
+}
+
+std::string ConfigManager::getRomName() {
+    return std::string();
+}
+
+int ConfigManager::getRed() {
+    return 0;
+}
+
+int ConfigManager::getGreen() {
+    return 0;
+}
+
+int ConfigManager::getBlue() {
+    return 0;
+}
--- Chip8/ConfigManager.h
+++ Chip8/ConfigManager.h
@@ -1,4 +1,30 @@
 #pragma once
+#include <string>
+
 class ConfigManager {
+public:
+	// Returns false if config.ini file not found.
+	bool loadConfigFile();
+
+	int getCycleRefreshRate();
+
+	std::string getRomName();
+
+	int getRed();
+	int getGreen();
+	int getBlue();
+
+private:
+	int scale;
+
+	// Cycle refresh rate is in Hertz. 
+	int CycleRefreshRate;
+
+	std::string romName;
+
+	// RGB values;
+	int rgbRed;
+	int rgbGreen;
+	int rgbBlue;
 };
 
